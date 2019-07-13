f8449e9d17ce786ab5a177b1f5c9f86a0fa75693
BS\Bartosz <staniszbartosz@gmail.com>
2019-07-13 20:02:59 +0200

    Add encrypt and decrypt functionalities
    Signed-off-by: BS\Bartosz <staniszbartosz@gmail.com>

--- spring-cloud-sample-config/src/main/java/moje/spring/cloud/sample/config/App.java
+++ spring-cloud-sample-config/src/main/java/moje/spring/cloud/sample/config/App.java
@@ -1,3 +1,5 @@
+package moje.spring.cloud.sample.config;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
